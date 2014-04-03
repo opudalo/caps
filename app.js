8c97c8aba010cc0ca0c3654931b2b0907c85c16e
Eugene Chechurin <e.chechurin@gmail.com>
2014-04-03 12:18:31 +0300

    add static serving

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var koa = require('koa'),
   hbs = require('koa-hbs'),
-  route = require('koa-route')
+  route = require('koa-route'),
+  serve = require('koa-static')
+
 
 var app = koa()
 
-app.use(hbs.middleware({ viewPath: __dirname + '/views' }))
 
-app.use(route.get('/', render))
+app.use(serve('public'))
+  .use(hbs.middleware({ viewPath: __dirname + '/pages' }))
+  .use(route.get('/', render))
 
 function* render() {
   yield this.render('main', {title: 'koa-hbs', content: 'some'})
