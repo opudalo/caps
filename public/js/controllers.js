7da461d09f2bd16d6c423685c360e1d1290ac5a4
Eugene Chechurin <e.chechurin@gmail.com>
2014-04-11 00:34:45 +0300

    start

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,10 +7,12 @@ var capsApp = angular.module('capsApp', [])
 capsApp.controller('MainCtrl', function($scope, $http) {
   $http.get('data/people.json').success(function(data) {
     $scope.people = data
+    console.log(data)
   })
 
   $http.get('data/caps.json').success(function(data) {
     $scope.caps = data
+    console.log(data)
   })
 
 })
