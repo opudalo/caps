af401ea6c5b916755f24a92275f65f3be4c26525
Eugene Chechurin <e.chechurin@gmail.com>
2014-04-13 11:18:29 +0300

    give a cap form

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,15 +5,47 @@
 var capsApp = angular.module('capsApp', [])
 
 capsApp.controller('MainCtrl', function($scope, $http) {
-  $http.get('data/people.json').success(function(data) {
-    $scope.people = data
-    console.log(data)
-  })
+  $scope.give = {
+    'peopleId': null,
+    'capId': null,
+    'reason': ''
+  }
 
-  $http.get('data/caps.json').success(function(data) {
+  $http.get('data/people.json').success(onpeople)
+  $http.get('data/caps.json').success(oncaps)
+  
+  $scope.setGiveItem = setGiveItem
+  $scope.giveCap = giveCap
+
+  dev()
+
+  function dev() {
+    $scope.give = {
+      'peopleId': 1,
+      'capId': 1,
+      'reason': 'Some reason'
+    }
+  }
+
+
+  function onpeople(data) {
+    $scope.people = data
+  }
+
+  function oncaps(data) {
     $scope.caps = data
-    console.log(data)
-  })
+  }
+    
+  function setGiveItem(data) {
+    _.extend($scope.give, data)
+  }
+
+  function giveCap() {
+    var d = $scope.give
+    if (d.peopleId == null || d.capId == null || !d.reason) return
+    console.log(d)
+  }
+
 
 })
 
