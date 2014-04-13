47e4a151fcf87544b60ff08f1ba40cac8faba1fd
Eugene Chechurin <e.chechurin@gmail.com>
2014-04-13 17:23:52 +0300

    styles for form

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,29 +2,64 @@
 
 /* Controllers */
 
-var capsApp = angular.module('capsApp', [])
+var app = angular.module('app', [])
 
-capsApp.filter('reverse', function() {
+app.filter('reverse', function() {
   return function(items) {
     return items.slice().reverse()
   }
-});
+})
 
-capsApp.controller('MainCtrl', function($scope, $http) {
-  $scope.give = {
-    'peopleId': null,
-    'capId': null,
-    'reason': ''
-  }
-  $scope.giveShown = false
+app.directive('ng-placeholder', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attr, ctrl) {      
+      
+      var value;
+      
+      var placehold = function () {
+          element.val(attr.placehold)
+      };
+      var unplacehold = function () {
+          element.val('');
+      };
+      
+      scope.$watch(attr.ngModel, function (val) {
+        value = val || '';
+      });
+
+      element.bind('focus', function () {
+         if(value == '') unplacehold();
+      });
+      
+      element.bind('blur', function () {
+         if (element.val() == '') placehold();
+      });
+      
+      ctrl.$formatters.unshift(function (val) {
+        if (!val) {
+          placehold();
+          value = '';
+          return attr.placehold;
+        }
+        return val;
+      });
+    }
+  };
+})
+
+app.controller('MainCtrl', function($scope, $http) {
 
   $http.get('data/people.json').success(onpeople)
   $http.get('data/caps.json').success(oncaps)
   
-  $scope.setGiveItem = setGiveItem
+  $scope.give = {}
+  $scope.setFormItem = setFormItem
   $scope.giveCap = giveCap
-  $scope.showGive = showGive
+  $scope.showForm = showForm
 
+  resetForm()
  // dev()
 
   function dev() {
@@ -36,6 +71,34 @@ capsApp.controller('MainCtrl', function($scope, $http) {
   }
 
 
+  function resetForm() {
+    $scope.give = {
+      'peopleId': null,
+      'capId': null,
+      'reason': ''
+    }
+    setPlaceholder()
+    $scope.formShown = false
+  }
+
+  function setPlaceholder() {
+    var capName = '',
+      peopleName = '',
+      give = $scope.give
+  
+    if(give.capId != null) {
+      capName = $scope.caps && $scope.caps[give.capId].name
+    }
+
+    if(give.peopleId != null) {
+      peopleName = $scope.people && (' ' + $scope.people[give.peopleId].name + ' for')
+    }
+
+    give.placeholder = ["This", capName + "cap", "is given to" + peopleName + "…"].join(' ')
+      
+    console.log($scope.give)
+  }
+
   function onpeople(data) {
     $scope.people = data
   }
@@ -44,12 +107,19 @@ capsApp.controller('MainCtrl', function($scope, $http) {
     $scope.caps = data
   }
     
-  function setGiveItem(data) {
-    _.extend($scope.give, data)
+  function setFormItem(data) {
+    var give = $scope.give
+    _.extend(give, data)
+
+    setPlaceholder()
   }
 
-  function showGive() {
-    $scope.giveShown = true
+  function showForm() {
+    $scope.formShown = true
+  }
+
+  function hideForm() {
+    resetForm()
   }
 
   function giveCap() {
@@ -69,7 +139,8 @@ capsApp.controller('MainCtrl', function($scope, $http) {
       data: $scope.people
     })
     console.log($scope.people)
-    $scope.giveShown = false
+
+    hideForm()
   }
 
 
