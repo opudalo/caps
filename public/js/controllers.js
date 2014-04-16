6d525d9f7f013e4f3b755e7036f813d7a4ad6cdb
Eugene Chechurin <e.chechurin@gmail.com>
2014-04-16 11:42:35 +0300

    capses collection

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,6 +6,7 @@ var app = angular.module('app', [])
 
 app.filter('reverse', function() {
   return function(items) {
+    if (!items || !items.length) return 
     return items.slice().reverse()
   }
 })
